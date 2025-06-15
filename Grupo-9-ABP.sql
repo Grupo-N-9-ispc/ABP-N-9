3bd6598ae4bf6bbec312cacdf5c9342d137d938e
Gonzalo vargas <gonzavargas687@gmail.com>
2025-06-15 18:37:19 -0300

    Add files via upload

---
+++ Grupo-9-ABP.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE sistema_de_usuarios;
+USE sistema_de_usuarios;
+CREATE TABLE roles (id INT AUTO_INCREMENT PRIMARY KEY,nombre_rol VARCHAR(50) NOT NULL UNIQUE);
+INSERT INTO roles (nombre_rol) VALUES 
+('admin'), 
+('usuario_estándar');
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    email VARCHAR(40) NOT NULL UNIQUE,
+    contraseña VARCHAR(30) NOT NULL,
+    rol_id INT NOT NULL,
+    estado ENUM('activo', 'inactivo') DEFAULT 'activo',
+    FOREIGN KEY (rol_id) REFERENCES roles(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE );
+INSERT INTO usuarios (nombre, email, contraseña, rol_id, estado)
+VALUES ('gonzalo vargas ', 'gonzavargas687@gmail.com', 'Gonzalo123', '1', 'activo');
+SELECT * FROM usuarios;
+SELECT * FROM usuarios WHERE estado = 'activo';
+SELECT * FROM usuarios WHERE id = 1;
+UPDATE usuarios SET estado = 'inactivo' WHERE id = 1;
+UPDATE usuarios SET email = 'gonzalovargas576@gmail.com' WHERE id = 1;
+DELETE FROM usuarios WHERE id = 1;
+INSERT INTO usuarios (nombre, email, contraseña, rol_id, estado)
+VALUES ('mariela  D´Aloia', 'marudaloia@gmail.com', 'Mar376', 'usuario_estándar', 'activo');
